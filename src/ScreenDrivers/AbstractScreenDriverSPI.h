e53b1ed53ac5d9ee72e2426642838fceb2f71bd0
GitMoDu <batatas@gmail.com>
2024-04-14 17:43:19 +0100

    WIP SPI base driver.

--- src/ScreenDrivers/AbstractScreenDriverSPI.h
+++ src/ScreenDrivers/AbstractScreenDriverSPI.h
@@ -26,9 +26,6 @@ protected:
 	static constexpr uint8_t SPIChannel = spiChannel;
 	static constexpr uint32_t SPISpeed = spiSpeed;
 
-	static constexpr uint32_t CLOCK_DIVIDER = F_CPU / SPISpeed;
-
-
 protected:
 	SPIClass SpiInstance;
 
@@ -47,36 +44,53 @@ public:
 	//TODO: Optional in-driver I2C begin.
 	virtual const bool Start()
 	{
-		digitalWrite(pinRST, LOW);
-		digitalWrite(pinDC, LOW);
-		digitalWrite(pinCS, LOW);
-		pinMode(pinRST, OUTPUT);
-		pinMode(pinDC, OUTPUT);
-		pinMode(pinCS, OUTPUT);
+		if (pinDC == UINT8_MAX
+			|| pinCS == UINT8_MAX)
+		{
+			return false;
+		}
 
 		PinReset();
 
 		//TODO: Optional in-driver SPI begin.
 #if defined(ARDUINO_ARCH_ESP32)
-		return SpiInstance.begin(pinCS, pinCLK, pinMOSI, -1);
-#elif defined(ARDUINO_ARCH_STM32F1)
-		SpiInstance.begin();
-		//SpiInstance.begin(BitOrder::LSBFIRST, 0);
-		/*	if (spiSpeed > 0)
-			{
-				SpiInstance.setClockDivider(CLOCK_DIVIDER);
-			}*/
-
-		return true;
+		if (pinCS != UINT8_MAX
+			&& pinCLK != UINT8_MAX
+			&& pinMOSI != UINT8_MAX)
+		{
+			SpiInstance.begin((int8_t)pinCLK, (int8_t)-1, (int8_t)pinMOSI, (int8_t)pinCS);
+		}
+		else if (pinCS != UINT8_MAX && pinCLK != UINT8_MAX)
+		{
+			SpiInstance.begin((int8_t)pinCLK, (int8_t)-1, (int8_t)-1, (int8_t)pinCS);
+		}
+		else if (pinCS != UINT8_MAX)
+		{
+			SpiInstance.begin((int8_t)-1, (int8_t)-1, (int8_t)-1, (int8_t)pinCS);
+		}
 #else
 		SpiInstance.begin();
-		if (spiSpeed > 0)
+#endif
+		return true;
+	}
+
+	virtual void Stop()
+	{
+		SpiInstance.end();
+		if (pinDC != UINT8_MAX)
 		{
-			SpiInstance.setClock(spiSpeed);
+			digitalWrite(pinDC, LOW);
 		}
 
-		return true;
-#endif
+		if (pinCS != UINT8_MAX)
+		{
+			digitalWrite(pinCS, HIGH);
+		}
+
+		if (pinRST != UINT8_MAX)
+		{
+			digitalWrite(pinRST, LOW);
+		}
 	}
 
 	virtual const uint8_t GetWidth() final
@@ -92,6 +106,11 @@ public:
 protected:
 	void PinReset()
 	{
+		pinMode(pinDC, OUTPUT);
+		digitalWrite(pinDC, LOW);
+		digitalWrite(pinCS, HIGH);
+		pinMode(pinCS, OUTPUT);
+
 		if (pinRST != UINT8_MAX)
 		{
 			digitalWrite(pinRST, HIGH);
@@ -102,5 +121,19 @@ protected:
 			digitalWrite(pinRST, HIGH);
 		}
 	}
-	};
+
+	void CommandStart(SPISettings& settings)
+	{
+		digitalWrite(pinCS, LOW);
+		digitalWrite(pinDC, LOW);
+		SpiInstance.beginTransaction(settings);
+	}
+
+	void CommandEnd()
+	{
+		SpiInstance.endTransaction();
+		digitalWrite(pinCS, HIGH);
+		digitalWrite(pinDC, LOW);
+	}
+};
 #endif

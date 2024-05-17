7f18ec9e5c96b26e1c3aa82686bcae567282af8d
GitMoDu <batatas@gmail.com>
2024-05-17 18:36:23 +0100

    Exposed sprite functionality.

--- src/ArduinoGraphicsDrawer.h
+++ src/ArduinoGraphicsDrawer.h
@@ -2,7 +2,9 @@
 #define _ARDUINO_GRAPHICS_DRAWER_INCLUDE_h
 
 #include "Model/IFrameDraw.h"
+#include "Model/ISprite.h"
 #include "Model/FontStyle.h"
+#include "Model/GraphicsBuffer.h"
 
 #include "Drawer/DrawState.h"
 #include "Drawer/ElementDrawer.h"
@@ -11,5 +13,26 @@
 #include "GraphicsRender/FontRenderer.h"
 #include "GraphicsRender/NumberRenderer.h"
 #include "GraphicsRender/TextRenderer.h"
+#include "GraphicsRender/SpriteRenderer.h"
+#include "GraphicsRender/SpriteFontRenderer.h"
+
+#include "Sprite/BitmapSprite.h"
+#include "Sprite/BitMaskSprite.h"
+
+#include "Sprite/Shader/SpriteShader.h"
+#include "Sprite/Shader/SpriteShaderEffect.h"
+
+#include "Sprite/Transform/InvertTransform.h"
+#include "Sprite/Transform/RotateTransform.h"
+#include "Sprite/Transform/SkewTransform.h"
+#include "Sprite/Transform/ScaleTransform.h"
+
+#include "Sprite/Equation/CircleSprite.h"
+#include "Sprite/Equation/RingSprite.h"
+#include "Sprite/Equation/RectangleSprite.h"
+
+#include "Sprite/Font/SpriteFont5x5.h"
+
+
 
 #endif

105e9ba86216cad1ce00e6fea89b0c3232bb333f
GitMoDu <batatas@gmail.com>
2024-08-07 10:26:34 +0100

    Clean-up color type.

--- src/Model/RgbColor.h
+++ src/Model/RgbColor.h
@@ -7,9 +7,9 @@
 
 struct RgbColor
 {
-	uint8_t r;
-	uint8_t g;
-	uint8_t b;
+	uint8_t r = 0;
+	uint8_t g = 0;
+	uint8_t b = 0;
 
 	RgbColor()
 		: r(0)
@@ -17,17 +17,31 @@ struct RgbColor
 		, b(0)
 	{}
 
+	RgbColor(const uint32_t rgb32)
+		: r((rgb32 >> 16)& UINT8_MAX)
+		, g((rgb32 >> 8)& UINT8_MAX)
+		, b((rgb32 >> 0)& UINT8_MAX)
+	{
+	}
+
 	RgbColor(const uint8_t _r, const uint8_t _g, const uint8_t _b)
 		: r(_r)
 		, g(_g)
 		, b(_b)
 	{}
 
+	void SetFrom(const RgbColor& color)
+	{
+		r = color.r;
+		g = color.g;
+		b = color.b;
+	}
+
 	void FromRGB32(const uint32_t rgb32)
 	{
 		r = (rgb32 >> 16) & UINT8_MAX;
-		g = (rgb32 >> 8) & UINT8_MAX;;
-		b = (rgb32 >> 0) & UINT8_MAX;;
+		g = (rgb32 >> 8) & UINT8_MAX;
+		b = (rgb32 >> 0) & UINT8_MAX;
 	}
 };
 #endif

cbad9699a5f2e7c26eff12811033804f4893e15b
GitMoDu <batatas@gmail.com>
2024-08-12 13:54:33 +0100

    Expanded Sprite font 3x5 to cover more symbols.

--- src/Sprite/Font/SpriteFont3x5.h
+++ src/Sprite/Font/SpriteFont3x5.h
@@ -17,6 +17,240 @@ namespace Font3x5
 	static constexpr uint8_t Height = 5;
 	static constexpr uint8_t Kerning = 1;
 
+	static constexpr uint8_t LetterA[Height] PROGMEM
+	{
+		0b01000000,
+		0b10100000,
+		0b11100000,
+		0b10100000,
+		0b10100000
+	};
+
+	static constexpr uint8_t LetterB[Height] PROGMEM
+	{
+		0b11000000,
+		0b10100000,
+		0b11000000,
+		0b10100000,
+		0b11000000
+	};
+
+	static constexpr uint8_t LetterC[Height] PROGMEM
+	{
+		0b01100000,
+		0b10000000,
+		0b10000000,
+		0b10000000,
+		0b01100000
+	};
+
+	static constexpr uint8_t LetterD[Height] PROGMEM
+	{
+		0b11000000,
+		0b10100000,
+		0b10100000,
+		0b10100000,
+		0b11000000
+	};
+
+	static constexpr uint8_t LetterE[Height] PROGMEM
+	{
+		0b11100000,
+		0b10000000,
+		0b11000000,
+		0b10000000,
+		0b11100000
+	};
+
+	static constexpr uint8_t LetterF[Height] PROGMEM
+	{
+		0b11100000,
+		0b10000000,
+		0b11000000,
+		0b10000000,
+		0b10000000
+	};
+
+	static constexpr uint8_t LetterG[Height] PROGMEM
+	{
+		0b01100000,
+		0b10000000,
+		0b10100000,
+		0b10100000,
+		0b01000000
+	};
+
+	static constexpr uint8_t LetterH[Height] PROGMEM
+	{
+		0b10100000,
+		0b10100000,
+		0b11100000,
+		0b10100000,
+		0b10100000
+	};
+
+	static constexpr uint8_t LetterI[Height] PROGMEM
+	{
+		0b11100000,
+		0b01000000,
+		0b01000000,
+		0b01000000,
+		0b11100000
+	};
+
+	static constexpr uint8_t LetterJ[Height] PROGMEM
+	{
+		0b11100000,
+		0b00100000,
+		0b00100000,
+		0b10100000,
+		0b01000000
+	};
+
+	static constexpr uint8_t LetterK[Height] PROGMEM
+	{
+		0b10100000,
+		0b10100000,
+		0b11000000,
+		0b10100000,
+		0b10100000
+	};
+
+	static constexpr uint8_t LetterL[Height] PROGMEM
+	{
+		0b10000000,
+		0b10000000,
+		0b10000000,
+		0b10000000,
+		0b11100000
+	};
+
+	static constexpr uint8_t LetterM[Height] PROGMEM
+	{
+		0b10100000,
+		0b11100000,
+		0b11100000,
+		0b10100000,
+		0b10100000
+	};
+
+	static constexpr uint8_t LetterN[Height] PROGMEM
+	{
+		0b10100000,
+		0b10100000,
+		0b10100000,
+		0b11100000,
+		0b10100000
+	};
+
+	static constexpr uint8_t LetterO[Height] PROGMEM
+	{
+		0b01000000,
+		0b10100000,
+		0b10100000,
+		0b10100000,
+		0b01000000
+	};
+
+	static constexpr uint8_t LetterP[Height] PROGMEM
+	{
+		0b11000000,
+		0b10100000,
+		0b11000000,
+		0b10000000,
+		0b10000000
+	};
+
+	static constexpr uint8_t LetterQ[Height] PROGMEM
+	{
+		0b01000000,
+		0b10100000,
+		0b10100000,
+		0b11100000,
+		0b01100000
+	};
+
+	static constexpr uint8_t LetterR[Height] PROGMEM
+	{
+		0b11000000,
+		0b10100000,
+		0b11000000,
+		0b10100000,
+		0b10100000
+	};
+
+	static constexpr uint8_t LetterS[Height] PROGMEM
+	{
+		0b01100000,
+		0b10000000,
+		0b01000000,
+		0b00100000,
+		0b11000000
+	};
+
+	static constexpr uint8_t LetterT[Height] PROGMEM
+	{
+		0b11100000,
+		0b01000000,
+		0b01000000,
+		0b01000000,
+		0b01000000
+	};
+
+	static constexpr uint8_t LetterU[Height] PROGMEM
+	{
+		0b10100000,
+		0b10100000,
+		0b10100000,
+		0b10100000,
+		0b01100000
+	};
+
+	static constexpr uint8_t LetterV[Height] PROGMEM
+	{
+		0b10100000,
+		0b10100000,
+		0b10100000,
+		0b10100000,
+		0b01000000
+	};
+
+	static constexpr uint8_t LetterW[Height] PROGMEM
+	{
+		0b10100000,
+		0b10100000,
+		0b11100000,
+		0b11100000,
+		0b01000000
+	};
+
+	static constexpr uint8_t LetterX[Height] PROGMEM
+	{
+		0b10100000,
+		0b10100000,
+		0b01000000,
+		0b10100000,
+		0b10100000
+	};
+
+	static constexpr uint8_t LetterY[Height] PROGMEM
+	{
+		0b10100000,
+		0b10100000,
+		0b01000000,
+		0b01000000,
+		0b01000000
+	};
+
+	static constexpr uint8_t LetterZ[Height] PROGMEM
+	{
+		0b11100000,
+		0b00100000,
+		0b01000000,
+		0b10000000,
+		0b11100000
+	};
+
 	static constexpr uint8_t Number0[Height] PROGMEM
 	{
 		0b11100000,
@@ -25,7 +259,6 @@ namespace Font3x5
 		0b10100000,
 		0b11100000
 	};
-
 
 	static constexpr uint8_t Number1[Height] PROGMEM
 	{
@@ -40,7 +273,7 @@ namespace Font3x5
 	{
 		0b11100000,
 		0b00100000,
-		0b01000000,
+		0b01100000,
 		0b10000000,
 		0b11100000
 	};
@@ -152,6 +385,86 @@ namespace Font3x5
 		0b11100000,
 		0b00000000
 	};
+
+	static constexpr uint8_t SymbolExclamation[Height] PROGMEM
+	{
+		0b01000000,
+		0b01000000,
+		0b01000000,
+		0b00000000,
+		0b01000000
+	};
+	static constexpr uint8_t SymbolQuestion[Height] PROGMEM
+	{
+		0b01000000,
+		0b10100000,
+		0b00100000,
+		0b00000000,
+		0b01000000
+	};
+
+	static constexpr uint8_t SymbolPercent[Height] PROGMEM
+	{
+		0b10000000,
+		0b00100000,
+		0b01000000,
+		0b10000000,
+		0b00100000
+	};
+
+	static constexpr uint8_t SymbolParenthesisOpen[Height] PROGMEM
+	{
+		0b01000000,
+		0b10000000,
+		0b10000000,
+		0b10000000,
+		0b01000000
+	};
+
+	static constexpr uint8_t SymbolParenthesisClose[Height] PROGMEM
+	{
+		0b01000000,
+		0b00100000,
+		0b00100000,
+		0b00100000,
+		0b01000000
+	};
+
+	static constexpr uint8_t SymbolBracketsOpen[Height] PROGMEM
+	{
+		0b11000000,
+		0b10000000,
+		0b10000000,
+		0b10000000,
+		0b11000000
+	};
+
+	static constexpr uint8_t SymbolBracketsClose[Height] PROGMEM
+	{
+		0b01100000,
+		0b00100000,
+		0b00100000,
+		0b00100000,
+		0b01100000
+	};
+
+	static constexpr uint8_t SymbolSmaller[Height] PROGMEM
+	{
+		0b00100000,
+		0b01000000,
+		0b10000000,
+		0b01000000,
+		0b00100000
+	};
+
+	static constexpr uint8_t SymbolBigger[Height] PROGMEM
+	{
+		0b10000000,
+		0b01000000,
+		0b00100000,
+		0b01000000,
+		0b10000000
+	};
 };
 
 class SpriteFont3x5Renderer : public AbstractSpriteFontRenderer<FlashBitMaskSprite<Font3x5::Width, Font3x5::Height>, Font3x5::Kerning>
@@ -192,6 +505,84 @@ private:
 			return Font3x5::Number8;
 		case '9':
 			return Font3x5::Number9;
+		case 'a':
+		case 'A':
+			return Font3x5::LetterA;
+		case 'b':
+		case 'B':
+			return Font3x5::LetterB;
+		case 'c':
+		case 'C':
+			return Font3x5::LetterC;
+		case 'd':
+		case 'D':
+			return Font3x5::LetterD;
+		case 'e':
+		case 'E':
+			return Font3x5::LetterE;
+		case 'f':
+		case 'F':
+			return Font3x5::LetterF;
+		case 'g':
+		case 'G':
+			return Font3x5::LetterG;
+		case 'h':
+		case 'H':
+			return Font3x5::LetterH;
+		case 'i':
+		case 'I':
+			return Font3x5::LetterI;
+		case 'j':
+		case 'J':
+			return Font3x5::LetterJ;
+		case 'k':
+		case 'K':
+			return Font3x5::LetterK;
+		case 'l':
+		case 'L':
+			return Font3x5::LetterL;
+		case 'm':
+		case 'M':
+			return Font3x5::LetterM;
+		case 'n':
+		case 'N':
+			return Font3x5::LetterN;
+		case 'o':
+		case 'O':
+			return Font3x5::LetterO;
+		case 'p':
+		case 'P':
+			return Font3x5::LetterP;
+		case 'q':
+		case 'Q':
+			return Font3x5::LetterQ;
+		case 'r':
+		case 'R':
+			return Font3x5::LetterR;
+		case 's':
+		case 'S':
+			return Font3x5::LetterS;
+		case 't':
+		case 'T':
+			return Font3x5::LetterT;
+		case 'u':
+		case 'U':
+			return Font3x5::LetterU;
+		case 'v':
+		case 'V':
+			return Font3x5::LetterV;
+		case 'w':
+		case 'W':
+			return Font3x5::LetterW;
+		case 'y':
+		case 'Y':
+			return Font3x5::LetterY;
+		case 'x':
+		case 'X':
+			return Font3x5::LetterX;
+		case 'z':
+		case 'Z':
+			return Font3x5::LetterZ;
 		case '.':
 			return Font3x5::SymbolDecimalPoint;
 		case ',':
@@ -202,6 +593,24 @@ private:
 			return Font3x5::SymbolPlus;
 		case '-':
 			return Font3x5::SymbolMinus;
+		case '!':
+			return Font3x5::SymbolExclamation;
+		case '?':
+			return Font3x5::SymbolQuestion;
+		case '%':
+			return Font3x5::SymbolPercent;
+		case '(':
+			return Font3x5::SymbolParenthesisOpen;
+		case ')':
+			return Font3x5::SymbolParenthesisClose;
+		case '[':
+			return Font3x5::SymbolBracketsOpen;
+		case ']':
+			return Font3x5::SymbolBracketsClose;
+		case '<':
+			return Font3x5::SymbolSmaller;
+		case '>':
+			return Font3x5::SymbolBigger;
 		default:
 			return nullptr;
 		}

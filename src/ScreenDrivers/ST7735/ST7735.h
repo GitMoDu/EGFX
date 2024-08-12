31eb20042b92693db9e9d5ec6a99b00065eb52a9
GitMoDu <batatas@gmail.com>
2024-08-12 17:02:23 +0100

    Added support for ST7735 80x160.

---
+++ src/ScreenDrivers/ST7735/ST7735.h
@@ -0,0 +1,92 @@
+// ST7735.h
+
+#ifndef _ST7735_h
+#define _ST7735_h
+
+#include <stdint.h>
+
+namespace ST7735
+{
+	static constexpr uint8_t Width = 162;
+	static constexpr uint8_t Height = 132;
+
+	namespace ST7735S
+	{
+		static constexpr uint8_t Width = 160;
+		static constexpr uint8_t Height = 80;
+
+		static constexpr uint8_t XOffset = (ST7735::Width - ST7735S::Width) / 2;
+		static constexpr uint8_t YOffset = (ST7735::Height - ST7735S::Height) / 2;
+	}
+
+	static constexpr uint32_t RESET_DELAY_MICROS = 1000;
+	static constexpr uint8_t RESET_WAIT_MICROS = 10;
+
+	static constexpr uint32_t SpiMaxSpeed = 33333333; // Serial write clock cycle ~33 ns.
+
+	static constexpr uint32_t LimitedSpiSpeed(const uint32_t speed)
+	{
+		return ((speed < SpiMaxSpeed) * (speed)) | ((speed >= SpiMaxSpeed) * (SpiMaxSpeed));
+	}
+
+	enum class CommandEnum : uint8_t
+	{
+		Nop = 0x00,
+		SoftwareReset = 0x01,
+
+		SetSleepOn = 0x10,
+		SetSleepOff = 0x11,
+		PartialDisplayOn = 0x12,
+		PartialDisplayOff = 0x13,
+		SetInversionOn = 0x20,
+		SetInversionOff = 0x21,
+		SetGamma = 0x26,
+		SetDisplayOff = 0x28,
+		SetDisplayOn = 0x29,
+		SetIdleOff = 0x38,
+		SetIdleOn = 0x39,
+
+		SetColumnAddress = 0x2A,
+		SetRowAddress = 0x2B,
+		MemoryWrite = 0x2C,
+		MemoryRead = 0x2E,
+
+		SetPixelFormat = 0x3A,
+		SetMemoryDataAccessControl = 0x36,
+		TearingOff = 0x34,
+		SetTearing = 0x35,
+		FrameRateControl1 = 0xB1,
+		FrameRateControl2 = 0xB2,
+		FrameRateControl3 = 0xB3,
+		InversionControl = 0xB4,
+		DisplaySettings = 0xB6,
+
+		PowerControl1 = 0xC0,
+		PowerControl2 = 0xC1,
+		PowerControl3 = 0xC2,
+		PowerControl4 = 0xC3,
+		PowerControl5 = 0xC4,
+
+		PowerVmControl = 0xC5,
+		PowerLControl = 0xC7,
+
+		GatePumpClockFrequency = 0xFC,
+		SetGammaPositiveCorrection = 0xE0,
+		SetGammaNegativeCorrection = 0xE1,
+
+		NvmControlStatus = 0xD9,
+	};
+
+	static constexpr uint8_t GammaPositiveCorrection[]
+	{
+		0x02, 0x1c, 0x07, 0x12, 0x37, 0x32, 0x29, 0x2c,
+		0x29, 0x25, 0x2b, 0x39, 0x00, 0x01, 0x03, 0x10
+	};
+
+	static constexpr uint8_t GammaNegativeCorrection[]
+	{
+		0x03, 0x1d, 0x07, 0x06, 0x2E, 0x2C, 0x29, 0x2c,
+		0x2e, 0x2e, 0x37, 0x3f, 0x00, 0x00, 0x02, 0x10
+	};
+}
+#endif
---
+++ src/ScreenDrivers/ScreenDriverST7735_SPI.h
@@ -0,0 +1,295 @@
+// ScreenDriverST7735_SPI.h
+
+#ifndef _SCREEN_DRIVER_ST7735_SPI_h
+#define _SCREEN_DRIVER_ST7735_SPI_h
+
+#include "AbstractScreenDriverSPI.h"
+#include "TemplateScreenDriverRtos.h"
+#include "TemplateScreenDriverSpiAsync.h"
+#include "TemplateScreenDriverSpiDma.h"
+#include "ST7735/ST7735.h"
+
+template<typename pixel_t,
+	const uint8_t pinDC,
+	const uint8_t pinCS,
+	const uint8_t pinRST,
+	const uint8_t pinCLK,
+	const uint8_t pinMOSI,
+	const uint8_t spiChannel,
+	const uint32_t spiSpeed>
+class AbstractScreenDriverST7735S_SPI : public AbstractScreenDriverSPI<GraphicsBuffer::GetBufferSize<pixel_t>(ST7735::ST7735S::Width, ST7735::ST7735S::Height), ST7735::ST7735S::Width, ST7735::ST7735S::Height, pinDC, pinCS, pinRST, pinCLK, pinMOSI, spiChannel, spiSpeed>
+{
+private:
+	using BaseClass = AbstractScreenDriverSPI<GraphicsBuffer::GetBufferSize<pixel_t>(ST7735::ST7735S::Width, ST7735::ST7735S::Height), ST7735::ST7735S::Width, ST7735::ST7735S::Height, pinDC, pinCS, pinRST, pinCLK, pinMOSI, spiChannel, spiSpeed>;
+
+public:
+	using BaseClass::ScreenWidth;
+	using BaseClass::ScreenHeight;
+	using BaseClass::BufferSize;
+
+protected:
+	using BaseClass::SpiInstance;
+	using BaseClass::CommandStart;
+	using BaseClass::CommandEnd;
+	using BaseClass::PinReset;
+
+private:
+	SPISettings Settings;
+
+public:
+	AbstractScreenDriverST7735S_SPI()
+		: BaseClass()
+		, Settings(ST7735::LimitedSpiSpeed(spiSpeed), MSBFIRST, SPI_MODE0)
+	{}
+
+public:
+	virtual const bool Start()
+	{
+		if (sizeof(pixel_t) != sizeof(uint16_t))
+		{
+			return false;
+		}
+
+		if (BaseClass::Start())
+		{
+			PinReset(ST7735::RESET_DELAY_MICROS);
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::SoftwareReset);
+			CommandEnd();
+			delayMicroseconds(ST7735::RESET_WAIT_MICROS);
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::SetSleepOff);
+			CommandEnd();
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::SetDisplayOff);
+			CommandEnd();
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::FrameRateControl1);
+			digitalWrite(pinDC, HIGH);
+			SpiInstance.transfer(0x00);
+			SpiInstance.transfer(0b111111);
+			SpiInstance.transfer(0b111111);
+			CommandEnd();
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::FrameRateControl2);
+			digitalWrite(pinDC, HIGH);
+			SpiInstance.transfer(0b1111);
+			SpiInstance.transfer(0x01);
+			SpiInstance.transfer(0x01);
+			CommandEnd();
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::FrameRateControl3);
+			digitalWrite(pinDC, HIGH);
+			SpiInstance.transfer(0x05);
+			SpiInstance.transfer(0x3c);
+			SpiInstance.transfer(0x3c);
+			SpiInstance.transfer(0x05);
+			SpiInstance.transfer(0x3c);
+			SpiInstance.transfer(0x3c);
+			CommandEnd();
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::InversionControl);
+			digitalWrite(pinDC, HIGH);
+			SpiInstance.transfer(0x03);
+			CommandEnd();
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::PowerControl1);
+			digitalWrite(pinDC, HIGH);
+			SpiInstance.transfer(0b11111100);
+			SpiInstance.transfer(0x08);
+			SpiInstance.transfer(0b10);
+			CommandEnd();
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::PowerControl2);
+			digitalWrite(pinDC, HIGH);
+			SpiInstance.transfer(0xC0);
+			CommandEnd();
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::PowerControl3);
+			digitalWrite(pinDC, HIGH);
+			SpiInstance.transfer(0x0D);
+			SpiInstance.transfer(0x00);
+			CommandEnd();
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::PowerControl4);
+			digitalWrite(pinDC, HIGH);
+			SpiInstance.transfer(0x8D);
+			SpiInstance.transfer(0x2A);
+			CommandEnd();
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::PowerControl5);
+			digitalWrite(pinDC, HIGH);
+			SpiInstance.transfer(0x8D);
+			SpiInstance.transfer(0xEE);
+			CommandEnd();
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::GatePumpClockFrequency);
+			digitalWrite(pinDC, HIGH);
+			SpiInstance.transfer(0b11011000);
+			CommandEnd();
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::NvmControlStatus);
+			digitalWrite(pinDC, HIGH);
+			SpiInstance.transfer(0b01000000);
+			CommandEnd();
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::PowerVmControl);
+			digitalWrite(pinDC, HIGH);
+			SpiInstance.transfer(0b001111);
+			CommandEnd();
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::PowerLControl);
+			digitalWrite(pinDC, HIGH);
+			SpiInstance.transfer(0b10000);
+			CommandEnd();
+
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::SetGamma);
+			digitalWrite(pinDC, HIGH);
+			SpiInstance.transfer(0x08);
+			CommandEnd();
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::SetMemoryDataAccessControl);
+			digitalWrite(pinDC, HIGH);
+			SpiInstance.transfer(0b01101000);
+			CommandEnd();
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::SetPixelFormat);
+			digitalWrite(pinDC, HIGH);
+			SpiInstance.transfer(0x05);
+			CommandEnd();
+			delay(10);
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::SetGammaPositiveCorrection);
+			digitalWrite(pinDC, HIGH);
+			SpiInstance.transfer((uint8_t*)ST7735::GammaPositiveCorrection, sizeof(ST7735::GammaPositiveCorrection));
+			CommandEnd();
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::SetGammaNegativeCorrection);
+			digitalWrite(pinDC, HIGH);
+			SpiInstance.transfer((uint8_t*)ST7735::GammaNegativeCorrection, sizeof(ST7735::GammaNegativeCorrection));
+			CommandEnd();
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::SetColumnAddress);
+			digitalWrite(pinDC, HIGH);
+			SpiInstance.transfer(0x00);
+			SpiInstance.transfer(ST7735::ST7735S::XOffset);
+			SpiInstance.transfer(0x00);
+			SpiInstance.transfer(ST7735::ST7735S::Width - 1 + ST7735::ST7735S::XOffset);
+			CommandEnd();
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::SetRowAddress);
+			digitalWrite(pinDC, HIGH);
+			SpiInstance.transfer(0x00);
+			SpiInstance.transfer(ST7735::ST7735S::YOffset);
+			SpiInstance.transfer(0x00);
+			SpiInstance.transfer(ST7735::ST7735S::Height - 1 + ST7735::ST7735S::YOffset);
+			CommandEnd();
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::SetInversionOff);
+			CommandEnd();
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::TearingOff);
+			CommandEnd();
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::SetIdleOff);
+			CommandEnd();
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::PartialDisplayOff);
+			CommandEnd();
+			delay(10);
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::SetDisplayOn);
+			CommandEnd();
+
+			CommandStart(Settings);
+			SpiInstance.transfer((uint8_t)ST7735::CommandEnum::PartialDisplayOn);
+			CommandEnd();
+			delay(255);
+
+			return true;
+		}
+
+		return false;
+	}
+
+	virtual void StartBuffer()
+	{
+		CommandStart(Settings);
+		SpiInstance.transfer((uint8_t)ST7735::CommandEnum::MemoryWrite);
+		digitalWrite(pinDC, HIGH);
+	}
+};
+
+template<const uint8_t pinDC = UINT8_MAX,
+	const uint8_t pinCS = UINT8_MAX,
+	const uint8_t pinRST = UINT8_MAX,
+	const uint8_t pinCLK = UINT8_MAX,
+	const uint8_t pinMOSI = UINT8_MAX,
+	const uint8_t spiChannel = 0,
+	const uint32_t spiSpeed = 4000000>
+using ScreenDriverST7735S_160x80x16_SPI = AbstractScreenDriverST7735S_SPI<uint16_t, pinDC, pinCS, pinRST, pinCLK, pinMOSI, spiChannel, spiSpeed>;
+
+template<const uint8_t pinDC = UINT8_MAX,
+	const uint8_t pinCS = UINT8_MAX,
+	const uint8_t pinRST = UINT8_MAX,
+	const uint8_t pinCLK = UINT8_MAX,
+	const uint8_t pinMOSI = UINT8_MAX,
+	const uint8_t spiChannel = 0,
+	const uint32_t spiSpeed = 4000000,
+	const uint8_t spiChunkDivisor = 2>
+using ScreenDriverST7735S_160x80x16_SPI_Async = TemplateScreenDriverSpiAsync<ScreenDriverST7735S_160x80x16_SPI<pinDC, pinCS, pinRST, pinCLK, pinMOSI, spiChannel, spiSpeed>, spiChunkDivisor>;
+
+#if defined(TEMPLATE_SCREEN_DRIVER_SPI_DMA)
+template<const uint8_t pinDC = UINT8_MAX,
+	const uint8_t pinCS = UINT8_MAX,
+	const uint8_t pinRST = UINT8_MAX,
+	const uint8_t pinCLK = UINT8_MAX,
+	const uint8_t pinMOSI = UINT8_MAX,
+	const uint8_t spiChannel = 0,
+	const uint32_t spiSpeed = 4000000,
+	const uint32_t pushSleepDuration = 0>
+using ScreenDriverST7735S_160x80x16_SPI_Dma = TemplateScreenDriverSpiDma<ScreenDriverST7735S_160x80x16_SPI<pinDC, pinCS, pinRST, pinCLK, pinMOSI, spiChannel, spiSpeed>, pushSleepDuration>;
+#endif
+
+#if defined(TEMPLATE_SCREEN_DRIVER_RTOS)
+template<const uint8_t pinDC = UINT8_MAX,
+	const uint8_t pinCS = UINT8_MAX,
+	const uint8_t pinRST = UINT8_MAX,
+	const uint8_t pinCLK = UINT8_MAX,
+	const uint8_t pinMOSI = UINT8_MAX,
+	const uint8_t spiChannel = 0,
+	const uint32_t spiSpeed = 4000000,
+	uint32_t stackHeight = 1500,
+	portBASE_TYPE priority = 1>
+using ScreenDriverST7735S_160x80x16_SPI_Rtos = TemplateScreenDriverRtos<ScreenDriverST7735S_160x80x16_SPI<pinDC, pinCS, pinRST, pinCLK, pinMOSI, spiChannel, spiSpeed>, stackHeight, priority>;
+#endif
+#endif

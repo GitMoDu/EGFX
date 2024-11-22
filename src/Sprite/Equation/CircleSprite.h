23b16ba6f648918d45cea23236bad00cb73bd61b
GitMoDu <batatas@gmail.com>
2024-11-22 23:46:36 +0000

    Added fixed radius circle for faster rendering.

--- src/Sprite/Equation/CircleSprite.h
+++ src/Sprite/Equation/CircleSprite.h
@@ -5,6 +5,48 @@
 
 #include "../../Model/ISprite.h"
 
+template<const uint8_t Radius>
+class CircleFixedSprite : public AbstractSprite<Radius * 2, Radius * 2>
+{
+private:
+	static constexpr uint16_t GetRadiusPower(const uint8_t radius)
+	{
+		return ((radius > 1) * (((uint16_t)radius * radius) - 1)) | ((radius <= 1) * (((uint16_t)radius * radius)));
+	}
+
+private:
+	static constexpr uint16_t RadiusPow = GetRadiusPower(Radius);
+
+public:
+	CircleFixedSprite() : AbstractSprite<Radius * 2, Radius * 2>()
+	{}
+
+	const bool IsInsideCircle(const uint8_t x, const uint8_t y)
+	{
+		const int16_t dx = -(int16_t)Radius + x + 1;
+		const int16_t dy = -(int16_t)Radius + y + 1;
+		const uint16_t distancePow = (uint16_t)((dx * dx) + (dy * dy));
+
+		return distancePow <= RadiusPow;
+	}
+
+	virtual const bool Get(RgbColor& color, const uint8_t x, const uint8_t y)
+	{
+		if (IsInsideCircle(x, y))
+		{
+			color.r = UINT8_MAX;
+			color.g = UINT8_MAX;
+			color.b = UINT8_MAX;
+
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+};
+
 class CircleSprite : public ISprite
 {
 private:

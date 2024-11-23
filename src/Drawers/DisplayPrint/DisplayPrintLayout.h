05a272084a222055fc57d58d7fa2bf3d98dc97d0
GitMoDu <batatas@gmail.com>
2024-11-23 00:10:25 +0000

    Improved display layout.

--- src/Drawers/DisplayPrint/DisplayPrintLayout.h
+++ src/Drawers/DisplayPrint/DisplayPrintLayout.h
@@ -8,21 +8,23 @@
 
 template<uint8_t x, uint8_t y,
 	uint8_t width, uint8_t height,
-	typename FontRendererType = SpriteFont5x5Renderer>
+	typename FontRendererType = SpriteFont5x5Renderer,
+	uint8_t maxLineCount = UINT8_MAX,
+	uint8_t maxCharacterCount = UINT8_MAX>
 struct DisplayPrintLayout : public FontLayoutElement<x, y, width, height, FontRendererType::FontWidth(), FontRendererType::FontHeight(), FontRendererType::FontKerning()>
 {
 private:
 	static constexpr uint8_t SmallestUint8(const uint8_t a, const uint8_t b)
 	{
-		return (a * (a <= b)) | (b * (b < a));
+		return (a * (a <= b)) | (b * (a > b));
 	}
 
-	static constexpr uint8_t LinesCount(const uint8_t fontHeight, const uint8_t maxLineCount = UINT8_MAX)
+	static constexpr uint8_t LinesCount(const uint8_t fontHeight)
 	{
 		return SmallestUint8(((height - (fontHeight + 1)) / (fontHeight + 1)), maxLineCount);
 	}
 
-	static constexpr uint8_t CharactersCount(const uint8_t fontWidth, const uint8_t maxCharacterCount = UINT8_MAX)
+	static constexpr uint8_t CharactersCount(const uint8_t fontWidth)
 	{
 		return SmallestUint8(width / (fontWidth + 1), maxCharacterCount);
 	}

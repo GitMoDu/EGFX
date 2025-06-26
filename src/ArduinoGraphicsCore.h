bc626b286ca8dfdc6312bab00352e438e91de2c9
GitMoDu <batatas@gmail.com>
2025-06-26 19:54:22 +0100

    Clean-up includes.

--- src/ArduinoGraphicsCore.h
+++ src/ArduinoGraphicsCore.h
@@ -8,10 +8,11 @@
 #include "Model/IFrameEngine.h"
 #include "Model/IScreenDriver.h"
 
-#include "FrameBuffer/Color16FrameBuffer.h"
-#include "FrameBuffer/Color8FrameBuffer.h"
 #include "FrameBuffer/MonochromeFrameBuffer.h"
 #include "FrameBuffer/BinaryFrameBuffer.h"
+#include "FrameBuffer/Color8FrameBuffer.h"
+#include "FrameBuffer/Color16FrameBuffer.h"
+#include "FrameBuffer/Color32FrameBuffer.h"
 
 #include "ArduinoGraphicsDrawer.h"
 #endif

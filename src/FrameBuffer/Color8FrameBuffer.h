012735368a1ebcb66bc4ef4abfbbf35eba1f24c7
GitMoDu <batatas@gmail.com>
2025-06-26 19:50:05 +0100

    Added optimized calls to 8bit framebuffer.

--- src/FrameBuffer/Color8FrameBuffer.h
+++ src/FrameBuffer/Color8FrameBuffer.h
@@ -85,6 +85,21 @@ namespace Egfx
 		/// <summary>
 		/// Optimized version for color_t = uint8_t.
 		/// </summary>
+		virtual void LineVerticalRaw(const color_t rawColor, const pixel_t x, const pixel_t y1, const pixel_t y2)
+		{
+			constexpr pixel_index_t lineSize = sizeof(color_t) * frameWidth;
+			const uint8_t value = Inverted ? ~rawColor : rawColor;
+			pixel_index_t offset = (sizeof(color_t) * frameWidth * y1) + (sizeof(color_t) * x);
+
+			for (pixel_t i = y1; i <= y2; i++, offset += lineSize)
+			{
+				Buffer[offset] = value;
+			}
+		}
+
+		/// <summary>
+		/// Optimized version for color_t = uint8_t.
+		/// </summary>
 		virtual void LineHorizontalRaw(const color_t rawColor, const pixel_t x1, const pixel_t y, const pixel_t x2)
 		{
 			const pixel_index_t offset = (sizeof(color_t) * frameWidth * y) + (sizeof(color_t) * x1);
@@ -97,6 +112,22 @@ namespace Egfx
 				memset(&Buffer[offset], rawColor, x2 - x1 + 1);
 			}
 		}
+
+		/// <summary>
+		/// Optimized version for color_t = uint8_t.
+		/// </summary>
+		virtual void RectangleFillRaw(const color_t rawColor, const pixel_t x1, const pixel_t y1, const pixel_t x2, const pixel_t y2)
+		{
+			const pixel_t lineStart = (sizeof(color_t) * x1);
+			const uint8_t value = Inverted ? ~rawColor : rawColor;
+			const uint16_t width = x2 - x1 + 1;
+
+			for (pixel_t y = y1; y <= y2; y++)
+			{
+				const pixel_index_t offset = (sizeof(color_t) * frameWidth * y) + lineStart;
+				memset(&Buffer[offset], value, width);
+			}
+		}*/
 	};
 }
 #endif

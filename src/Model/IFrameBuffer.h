3fbb0b834424f329ce7385c3cc86b8a6ebc99cb2
GitMoDu <batatas@gmail.com>
2025-08-09 19:28:43 +0100

    Updated interface for a more clear API.

--- src/Model/IFrameBuffer.h
+++ src/Model/IFrameBuffer.h
@@ -67,84 +67,123 @@ namespace Egfx
 		/// <summary>
 		/// Fill the whole frame with the provided color.
 		/// </summary>
-		/// <param name="color"></param>
+		/// <param name="color">The color for filling.</param>
 		virtual void Fill(const rgb_color_t color) = 0;
 
 		/// <summary>
 		/// Draw a single pixel.
 		/// </summary>
-		/// <param name="color"></param>
-		/// <param name="x"></param>
-		/// <param name="y"></param>
+		/// <param name="color">The pixel color.</param>
 		virtual void Pixel(const rgb_color_t color, const pixel_t x, const pixel_t y) = 0;
+
+		/// <summary>
+		/// Draw a single pixel.
+		/// </summary>
+		/// <param name="color">The pixel color.</param>
 		virtual void Pixel(const rgb_color_t color, const pixel_point_t point) = 0;
 
+		/// <summary>
+		/// Blend a pixel color with 50/50 split.
+		/// </summary>
+		/// <param name="color">The color to blend with the pixel.</param>
 		virtual void PixelBlend(const rgb_color_t color, const pixel_t x, const pixel_t y) = 0;
+
+		/// <summary>
+		/// Blend a pixel color with given alpha split.
+		/// </summary>
+		/// <param name="color">The color to blend with the pixel.</param>
+		/// <param name="alpha">The alpha value (opacity) to use for blending, typically in the range 0 (fully transparent) to 255 (fully opaque).</param>
 		virtual void PixelBlendAlpha(const rgb_color_t color, const pixel_t x, const pixel_t y, const uint8_t alpha) = 0;
+
+		/// <summary>
+		/// Blend a pixel color using color add.
+		/// </summary>
+		/// <param name="color">The color to add into the pixel.</param>
 		virtual void PixelBlendAdd(const rgb_color_t color, const pixel_t x, const pixel_t y) = 0;
+
+		/// <summary>
+		/// Blend a pixel color using color subtract.
+		/// </summary>
+		/// <param name="color">The color to subtract from the pixel.</param>
 		virtual void PixelBlendSubtract(const rgb_color_t color, const pixel_t x, const pixel_t y) = 0;
+
+		/// <summary>
+		/// Blend a pixel color using color multiply.
+		/// </summary>
+		/// <param name="color">The color to multiply with the pixel.</param>
 		virtual void PixelBlendMultiply(const rgb_color_t color, const pixel_t x, const pixel_t y) = 0;
+
+		/// <summary>
+		/// Blend a pixel color using color screen.
+		/// Screen blending is a special case of color addition that produces a lighter color.
+		/// </summary>
+		/// <param name="color">The color to screen with the pixel.</param>
 		virtual void PixelBlendScreen(const rgb_color_t color, const pixel_t x, const pixel_t y) = 0;
 
 		/// <summary>
 		/// Draw a line. 
 		/// Special orientations (horizontal and vertical) are internally optimized.
 		/// </summary>
-		/// <param name="color"></param>
-		/// <param name="x1"></param>
-		/// <param name="y1"></param>
-		/// <param name="x2"></param>
-		/// <param name="y2"></param>
+		/// <param name="color">The line color.</param>
 		virtual void Line(const rgb_color_t color, const pixel_t x1, const pixel_t y1, const pixel_t x2, const pixel_t y2) = 0;
+
+		/// <summary>
+		/// Draw a line. 
+		/// Special orientations (horizontal and vertical) are internally optimized.
+		/// </summary>
+		/// <param name="color">The line color.</param>
 		virtual void Line(const rgb_color_t color, const pixel_line_t& line) = 0;
 
 		/// <summary>
 		/// Draw a rectangle outline.
 		/// </summary>
-		/// <param name="color"></param>
-		/// <param name="x1"></param>
-		/// <param name="y1"></param>
-		/// <param name="x2"></param>
-		/// <param name="y2"></param>
-		virtual void Rectangle(const rgb_color_t color, const pixel_t x1, const pixel_t y1, const pixel_t x2, const pixel_t y2) = 0;
+		/// <param name="color">The outline color.</param>
 		virtual void Rectangle(const rgb_color_t color, const pixel_rectangle_t& rectangle) = 0;
+
+		/// <summary>
+		/// Draw a rectangle outline.
+		/// </summary>
+		/// <param name="color">The outline color.</param>
+		virtual void Rectangle(const rgb_color_t color,
+			const pixel_t topLeftX, const pixel_t topLeftY,
+			const pixel_t bottomRightX, const pixel_t bottomRightY) = 0;
 
 		/// <summary>
 		/// Draw a full rectangle.
 		/// </summary>
-		/// <param name="color"></param>
-		/// <param name="x1"></param>
-		/// <param name="y1"></param>
-		/// <param name="x2"></param>
-		/// <param name="y2"></param>
-		virtual void RectangleFill(const rgb_color_t color, const pixel_t x1, const pixel_t y1, const pixel_t x2, const pixel_t y2) = 0;
+		/// <param name="color">The fill color.</param>
 		virtual void RectangleFill(const rgb_color_t color, const pixel_rectangle_t& rectangle) = 0;
+
+		/// <summary>
+		/// Draw a full rectangle.
+		/// </summary>
+		/// <param name="color">The fill color.</param>
+		virtual void RectangleFill(const rgb_color_t color,
+			const pixel_t topLeftX, const pixel_t topLeftY,
+			const pixel_t bottomRightX, const pixel_t bottomRightY) = 0;
 
 		/// <summary>
 		/// Draw a triangle outline.
 		/// </summary>
-		/// <param name="color"></param>
-		/// <param name="x1"></param>
-		/// <param name="y1"></param>
-		/// <param name="x2"></param>
-		/// <param name="y2"></param>
-		/// <param name="x3"></param>
-		/// <param name="y3"></param>
+		/// <param name="color">The outline color.</param>
 		virtual void Triangle(const rgb_color_t color, const pixel_t x1, const pixel_t y1, const pixel_t x2, const pixel_t y2, const pixel_t x3, const pixel_t y3) = 0;
-		virtual void Triangle(const rgb_color_t color, const pixel_triangle_t& triangle) = 0;
 
+		/// <summary>
+		/// Draw a triangle outline.
+		/// </summary>
+		/// <param name="color">The outline color.</param>
+		virtual void Triangle(const rgb_color_t color, const pixel_triangle_t& triangle) = 0;
 
 		/// <summary>
 		/// Draw a full triangle.
 		/// </summary>
-		/// <param name="color"></param>
-		/// <param name="x1"></param>
-		/// <param name="y1"></param>
-		/// <param name="x2"></param>
-		/// <param name="y2"></param>
-		/// <param name="x3"></param>
-		/// <param name="y3"></param>
+		/// <param name="color">The fill color.</param>
 		virtual void TriangleFill(const rgb_color_t color, const pixel_t x1, const pixel_t y1, const pixel_t x2, const pixel_t y2, const pixel_t x3, const pixel_t y3) = 0;
+
+		/// <summary>
+		/// Draw a full triangle.
+		/// </summary>
+		/// <param name="color">The fill color.</param>
 		virtual void TriangleFill(const rgb_color_t color, const pixel_triangle_t& triangle) = 0;
 
 	};

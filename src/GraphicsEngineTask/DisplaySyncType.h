a28f1f901f58e59f03018b86075dff6d449c760f
GitModu <batatas@gmail.com>
2025-12-23 11:21:38 +0000

    Clean-up enum.

--- src/GraphicsEngineTask/DisplaySyncType.h
+++ src/GraphicsEngineTask/DisplaySyncType.h
@@ -1,24 +1,24 @@
-// DisplaySyncType.h
-
 #ifndef _DISPLAY_SYNC_TYPE_h
 #define _DISPLAY_SYNC_TYPE_h
 
 #include <stdint.h>
 
-enum class DisplaySyncType : uint8_t
+namespace Egfx
 {
-	/// <summary>
-	/// Sync with frame-rate multiples going down to 1/2, 1/4 and below to keep frame-pace.
-	/// </summary>
-	VSync,
-	/// <summary>
-	/// Uneven display sync with variable frame-rate to adjust for low frame-rates.
-	/// </summary>
-	Vrr,
-	/// <summary>
-	/// Render and display as fast as possible.
-	/// </summary>
-	NoSync
-};
-
+	enum class DisplaySyncType : uint8_t
+	{
+		/// <summary>
+		/// Sync with frame-rate multiples going down to 1/2, 1/4 and below to keep frame-pace.
+		/// </summary>
+		VSync,
+		/// <summary>
+		/// Uneven display sync with variable frame-rate to adjust for low frame-rates.
+		/// </summary>
+		Vrr,
+		/// <summary>
+		/// Render and display as fast as possible.
+		/// </summary>
+		NoSync
+	};
+}
 #endif
